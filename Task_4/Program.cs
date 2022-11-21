b2a2476418019814b93f5bd6ebd7e4e0cd8bdd9f
Geo-Schon <dr-gudkov@mail.ru>
2022-11-21 14:38:27 +0400

    Первая задача Tas4

---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numС = Convert.ToInt32(Console.ReadLine());
+    
+    if (numA > numB)
+    {
+        if (numA > numC)
+        {
+            Console.WriteLine("Первое число максимальное");
+        }
+        else
+        {
+            Console.WriteLine("Второе число максимальное");
+        }
+    }
+    else 
+    {
+        if (numB > numC)
+        {
+            Console.WriteLine("Второе число максимальное");
+        }
+        else
+        {
+            Console.WriteLine("Третье число максимальное");
+        }
+    }
