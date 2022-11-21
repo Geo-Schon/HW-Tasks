ac76df7d76786370f33fcee2e548fd35a22b0209
Geo-Schon <dr-gudkov@mail.ru>
2022-11-21 14:25:33 +0400

    Первая задача Task2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,21 +1,16 @@
 ﻿Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-if(num1 > num2)
-{
-Console.WriteLine("Первое число больше второго");
-}
-else if (num1 < num2)
- {
-Console.WriteLine("Первое число меньше второго");
-}
-else
-{
-                Console.WriteLine("Оба числа равны");
-            }
-            Console.ReadKey();
-        }
-    }
-}
+int numB = Convert.ToInt32(Console.ReadLine());
+    if(numA > numB)
+    {
+    Console.WriteLine("Первое число больше второго");
+    }
+     else if (numA < numB)
+     {
+     Console.WriteLine("Первое число меньше второго");
+    }
+    else
+     {
+    Console.WriteLine("Числа равны");
+    }   
