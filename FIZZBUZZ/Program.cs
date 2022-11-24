b9d0bfc06a9466900576579b7f9a384b83b26530
Geo-Schon <dr-gudkov@mail.ru>
2022-11-24 16:37:54 +0400

    fizzbuzz

--- FIZZBUZZ/Program.cs
+++ FIZZBUZZ/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿for (int inx =1; inx <= 100; inx++)
+{
+    string str = "";
+    if (i % 3 == 0)
+    {
+        str += "Fizz";
+    }
+    if (i % 5 == 0)
+    {
+        str += "Buzz";
+    }
+    if (str.length == 0)
+    {
+        str = i.ToString();
+    }
+    Console.WriteLine(str);
+}
