42c37550c861aa9b1b58d948b098a78320e31316
Geo-Schon <dr-gudkov@mail.ru>
2022-11-24 15:28:40 +0400

    Третья задача task_15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+
+if (int Day >= 7)
+{
+    Console.ReadLine("Число вне пределов 7 дней");
+}
+else
+{
+    {
+    if (int Day = 6 or 7)
+        Console.ReadLine("Это выходной день!");
+    }
+    else
+        Console.ReadLine("Это будний день!");
+    }
+}
