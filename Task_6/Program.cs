d18a796aa1f654a2a811ab19d741c16cb259ff34
Geo-Schon <dr-gudkov@mail.ru>
2022-11-24 15:49:41 +0400

    Первая  задача Task10

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-    if (a % 2==1)
+    if (num % 2 == 0)
         {
-        Console.WriteLine("Число нечетное");
+        Console.WriteLine("Число четное");
         }
     else
         {
-        Console.WriteLine("Число четное");
+        Console.WriteLine("Число нечетное");
         }
--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >=100)
+{
+    while (num >= 1000)
+        {
+        num /= 10;
+        }
+    int res = num % 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Нет");
+}
