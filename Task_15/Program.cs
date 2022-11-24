38f94b7b6b80eac92e70aa1c300ce4d536582c00
Geo-Schon <dr-gudkov@mail.ru>
2022-11-24 15:49:08 +0400

    Третья задача task_15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,17 +1,14 @@
 ﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
 int Day = Convert.ToInt32(Console.ReadLine());
 
-if (int Day >= 7)
+if (Day >= 7)
 {
-    Console.ReadLine("Число вне пределов 7 дней");
+    Console.WriteLine("Число вне пределов 7 дней");
 }
 else
 {
-    {
-    if (int Day = 6 or 7)
-        Console.ReadLine("Это выходной день!");
-    }
-    else
-        Console.ReadLine("Это будний день!");
-    }
+    if (Day >= 6)
+        Console.WriteLine("Это выходной день!");
+    if (Day < 5)
+        Console.WriteLine("Это будний день!");
 }
