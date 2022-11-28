bf4498436d828617197341b153f18450e2d8d8af
Geo-Schon <dr-gudkov@mail.ru>
2022-11-28 20:50:58 +0400

    Закончена Task_19

--- N-prog/Program.cs
+++ N-prog/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Сколько программистов в комнате: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+end_1 = "программист";
+end_2 = "программиста";
+end_3 = "программистов";
+
+if  num >=0
+{
+    if num == 0
+    {
+        Console.WriteLine("В комнате нет программистов");
+    }
+    if num = 1
+    {
+        Console.WriteLine(num end_1);
+    }
+    if num = (0,5)
+}
+  if s==0:
+    print(str(s) + n1)
+  elif s%100>=10 and s%100<=20:
+    print(str(s) + n1)
+  elif s%10==1:
+    print(str(s) + n2)
+  elif s%10>=2 and s%10<=4:
+    print(str(s) + n3)
+  else:
+    print(str(s) + n1)
---
+++ Task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿int num, rem, sum = 0, temp;
+Console.WriteLine("Введите пятизначное число");
+num = Convert.ToInt32(Console.ReadLine());
+    temp = num;
+    while (num > 0)
+    {
+        rem = num % 10;
+        num = num / 10;
+        sum = sum * 10 + rem;
+    }
+    if (temp == sum)
+    {
+    Console.WriteLine("Это число палиндром");
+    }
+    else
+    {
+    Console.WriteLine("Это число не палиндром");
+    }
+    Console.ReadLine();
+    
