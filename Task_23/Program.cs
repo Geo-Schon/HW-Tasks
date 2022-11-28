fad663df1f254d73251ed1ad67478dbbb98b42bb
Geo-Schon <dr-gudkov@mail.ru>
2022-11-28 21:36:24 +0400

    Закончена Task_23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int num_cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] num_cube)
+{
+  int counter = 0;
+  int length = num_cube.Length;
+  while (counter <  length)
+  {
+    num_cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] amount)
+{
+  int count = amount.Length;
+  int index = 1;
+  while(index < count)
+  {
+    Console.Write(amount[index]+ " ");
+    index++;
+  }
+} 
+int[] arry = new int[num_cube+1];
+Cube(arry);
+PrintArry(arry);
