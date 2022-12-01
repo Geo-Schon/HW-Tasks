085e9e0cddbd2c2ab65922577f01804c100da85d
Geo-Schon <dr-gudkov@mail.ru>
2022-12-01 14:09:50 +0400

    Сделана task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = ReadInt("Введите число: ");
+int degree = ReadInt("Введите степень: ");
+
+Console.WriteLine($"{num} * {degree} = " + Math.Pow(num, degree));
+
+// Если вам нужно получить целое число степени, то можете сделать так:
+
+// int i = Convert.ToInt32(Math.Pow(5, 3));
+
