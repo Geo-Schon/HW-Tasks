9c7b03976f25ec064a4fba04b7d2b81313c90ede
Geo-Schon <dr-gudkov@mail.ru>
2022-12-01 14:38:13 +0400

    Сделана task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+while (num>0)
+    {
+        counter++;
+        num = num / 10;
+    }
+Console.WriteLine("Данное число содержит " + counter + " цифры");
+
