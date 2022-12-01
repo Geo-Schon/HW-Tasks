248ba2c89f4daefd5fe99513d2f0515a0cb35c4d
Geo-Schon <dr-gudkov@mail.ru>
2022-12-01 15:52:44 +0400

    Сделана task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,8 @@
+﻿int LenArray = 8;
+int[] NewArray = new int[LenArray];
+for (int index = 0; index < NewArray.Length; index++)
+{
+    NewArray[index] = new Random().Next(1,100);
+    Console.Write(NewArray[index] + " ");
+}
+Console.Write (Convert.ToInt32(Console.ReadLine()));
