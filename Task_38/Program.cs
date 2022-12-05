3a931fe64294da7d9af74790ef58903be773e8b6
Geo-Schon <dr-gudkov@mail.ru>
2022-12-05 17:54:25 +0400

    Сделана task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите размер массива  ");
+int MassiveSize = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[MassiveSize];
+RandomNumbers(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int v = 0; v < numbers.Length; v++)
+{
+    if (numbers[v] > max)
+        {
+            max = numbers[v];
+        }
+    if (numbers[v] < min)
+        {
+            min = numbers[v];
+        }
+}
+
+Console.WriteLine($"В массиве {numbers.Length} чисел");
+Console.WriteLine ($"Максимальное значение: {max}; Минимальное значение: = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {max - min}");
+
+void RandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
