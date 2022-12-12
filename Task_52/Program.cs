4cdd99932492754dfd19835dd2ad8d12b9d05a85
Geo-Schon <dr-gudkov@mail.ru>
2022-12-12 16:27:16 +0400

    Сделана 52 задача

---
+++ Task_52/Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new Random();
+int[,] arr = new int[random.Next(4, 8), random.Next(4, 8)];
+Console.WriteLine ("Массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(" ");
+Console.WriteLine ("Среднее арифметическое элементов в каждом столбце: ");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    double spar = sum / arr.GetLength(0);
+    Console.Write($"{ Math.Round (sum / arr.GetLength(0),1)} ; ");
+}
+Console.ReadLine();
