9f9cdd6c27cfbe4255b0198b8f8d5e359f195287
Geo-Schon <dr-gudkov@mail.ru>
2022-12-15 21:05:29 +0400

    Сделана 60  задача

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,,] array3 = new int[2, 2, 2];
+FillArray(array3);
+PrintIndex(array3);
+
+void FillArray(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+void PrintIndex(int[,,] array)
+{
+    for (int i = 0; i < array3.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3.GetLength(2); k++)
+            {
+                Console.Write($" {array3[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
