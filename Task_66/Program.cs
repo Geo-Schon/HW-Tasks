66193da47c6b2917c6290b431cccc77aadd5d581
Geo-Schon <dr-gudkov@mail.ru>
2022-12-19 16:20:55 +0400

    Сделана 66 задача

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -1,32 +1,32 @@
-﻿// Console.Write("Введите число M: ");
-// int numM = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число N:");
-// int numN = Convert.ToInt32(Console.ReadLine());
-
-// void NumberSum (int numM, int numN, int sum)
-// {
-//     if (numM > numN) 
-//     {
-//         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
-//         return;
-//     }
-//     sum = sum + (numM++);
-//     NumberSum(numM, numN, sum);
-// }
-// NumberSum(numM, numN, 0);
-
-int NumberSum(int numM, int numN)
-{
-    if (numM == 0) return (numN * (numN + 1)) / 2;            
-    else if (numN == 0) return (numM * (numM + 1)) / 2;       
-    else if (numM == numN) return numM;            
-    else if (numM < numN) return numN + NumberSum(numM, numN - 1);
-    else return numN + NumberSum(numM, numN + 1);
-}
-
-Console.Write("Введите число M: ");
+﻿Console.Write("Введите число M: ");
 int numM = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число N: ");
+Console.Write("Введите число N:");
 int numN = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {NumberSum(numM, numN)}");
+
+void NumberSum (int numM, int numN, int sum)
+{
+    if (numM > numN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numM++);
+    NumberSum(numM, numN, sum);
+}
+NumberSum(numM, numN, 0);
+
+// int NumberSum(int numM, int numN)
+// {
+//     if (numM == 0) return (numN * (numN + 1)) / 2;            
+//     else if (numN == 0) return (numM * (numM + 1)) / 2;       
+//     else if (numM == numN) return numM;            
+//     else if (numM < numN) return numN + NumberSum(numM, numN - 1);
+//     else return numN + NumberSum(numM, numN + 1);
+// }
+
+// Console.Write("Введите число M: ");
+// int numM = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int numN = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {NumberSum(numM, numN)}");
 
