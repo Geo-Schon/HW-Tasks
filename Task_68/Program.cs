32608956be20bc8df7730fa2dd9e380071087755
Geo-Schon <dr-gudkov@mail.ru>
2022-12-19 16:37:52 +0400

    Сделана 68 задача

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return AckermanFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    return AckermanFunction(m, n);
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана: {AckermanFunction(m, n)}");
+
