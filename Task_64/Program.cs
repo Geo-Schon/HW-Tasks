19be0c4df0b50e3afafe0074b8c375c2be6a6e47
Geo-Schon <dr-gudkov@mail.ru>
2022-12-19 15:59:01 +0400

    Сделана 64 задача

---
+++ Task_64/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+void NumberCounter (int num)
+{
+    if (num < 0) Console.Write($"{num} не натуральное число");
+    if (num == 0) return;
+    Console.Write("{0} ", num);
+    NumberCounter (num - 1);
+}
+NumberCounter(num);
+
+// void NumberCounter(int num)
+// {
+//     if (num <= 0)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         Console.Write(num + " ");
+//         NumberCounter(num - 1);
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// NumberCounter(num);
+
